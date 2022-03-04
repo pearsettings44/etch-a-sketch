fb85e6848ebb289f3c8f78ffe7d47823fdf6f499
pearsettings44 <jose.a.pereira03@gmail.com>
2022-03-04 14:12:22 +0000

    Change grid size to 700px. Add title.

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ function createGrid(size) {
   for (let counter = 0; counter < size ** 2; counter++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    width = height = 800/size;
+    width = height = 700/size;
     cell.style.width = `${width}px`;
     cell.style.height = `${height}px`;
     cell.textContent = "";
     gridContainer.appendChild(cell);
   }
 }
+
+gridContainer.addEventListener('mouseover', e => e.target.classList.add('hover'))
 
 createGrid(16);
