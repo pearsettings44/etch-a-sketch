c5dc71891f349133e9e765d4f7bc865094b51c30
pearsettings44 <jose.a.pereira03@gmail.com>
2022-03-05 17:37:40 +0000

    Add comments.

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const gridContainer = document.querySelector("#grid-container");
 const gridContainerDim = gridContainer.clientWidth;
+const resetButton = document.querySelector("#reset");
+const blackButton = document.querySelector("#black");
+const rgbButton = document.querySelector("#rgb");
+const SixteenButton = document.querySelector("#size16");
+const ThirtyTwoButton = document.querySelector("#size32");
+const SixtyFourButton = document.querySelector("#size64");
 
 function createGrid(size) {
+  // createGrid(size): Creates a grid with dimensions size x size.
   for (let counter = 0; counter < size ** 2; counter++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
@@ -13,8 +20,62 @@ function createGrid(size) {
   }
 }
 
-gridContainer.addEventListener("mouseover", (e) =>
-  e.target.classList.add("hover")
-);
+function reset() {
+  // reset(): clears the grid.
+  cells = gridContainer.children;
+  for (let counter = 0; counter < cells.length; counter++) {
+    let cell = cells[counter];
+    // Removing the class hover will make the grid white again.
+    cell.classList.remove("hover");
+  }
+}
+
+function hover(e) {
+  //  hover(e): Add color to cell.
+  e.target.classList.add("hover");
+}
+
+function black() {
+  // black(): Make the color black
+  gridContainer.removeEventListener("mouseover", hover);
+  gridContainer.addEventListener("mouseover", hover);
+}
+
+resetButton.addEventListener("click", reset);
+blackButton.addEventListener("click", black);
+
+ThirtyTwoButton.addEventListener("click", function () {
+  // Makes the grid dimentions 32x32
+  if (gridContainer.children.length === 32 * 32) {
+    return;
+  }
+  while (gridContainer.firstChild) {
+    gridContainer.firstChild.remove();
+  }
+  createGrid(32);
+});
+
+SixtyFourButton.addEventListener("click", function () {
+  // Makes the grid dimentions 64x64
+  if (gridContainer.children.length === 64 * 64) {
+    return;
+  }
+  while (gridContainer.firstChild) {
+    gridContainer.firstChild.remove();
+  }
+  createGrid(64);
+});
+
+SixteenButton.addEventListener("click", function () {
+  // Makes the grid dimentions 16x16
+  if (gridContainer.children.length === 16 * 16) {
+    return;
+  }
+  while (gridContainer.firstChild) {
+    gridContainer.firstChild.remove();
+  }
+  createGrid(16);
+});
 
 createGrid(16);
+gridContainer.addEventListener("mouseover", hover);
