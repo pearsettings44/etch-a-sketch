b47f9279171b474c0d7515dd5f88846536f896ee
pearsettings44 <jose.a.pereira03@gmail.com>
2022-03-05 15:18:48 +0000

    Change container size on javascript.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const gridContainer = document.querySelector("#grid-container");
+const gridContainerDim = gridContainer.clientWidth;
 
 function createGrid(size) {
   for (let counter = 0; counter < size ** 2; counter++) {
     const cell = document.createElement("div");
     cell.classList.add("cell");
-    width = height = 700 / size;
+    width = height = gridContainerDim / size;
     cell.style.width = `${width}px`;
     cell.style.height = `${height}px`;
     cell.textContent = "";
